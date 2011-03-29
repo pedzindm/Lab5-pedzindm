33498155bf2e1467ac74e27bdefba97995bef9a2
unknown <pedzindm@.rose-hulman.edu>
2011-03-29 18:37:16 -0400

    test2

--- PrimeNumbers/src/PrimeNumbersTest.java
+++ PrimeNumbers/src/PrimeNumbersTest.java
@@ -1,4 +1,7 @@
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -8,5 +11,9 @@ public class PrimeNumbersTest {
 	@Test
 	public void testJUnit(){
 		assertTrue("the complier is feeling well today ...", true);
+	}
+	@Test
+	public void testOne(){
+		assertEquals(new ArrayList<Integer>(),PrimeNumbers.numberGenerator(1));
 	}
 }
